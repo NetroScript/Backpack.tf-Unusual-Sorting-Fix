0c5ee92c9168461594b0180d9caa0d1c2d0408ea
NetroScript <NetroScript@users.noreply.github.com>
2017-08-08 19:15:14 +0200

    Version 0.2
    Added:
    * Also sort on https://backpack.tf/unusuals
    * Fixed Filter items field

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # Temporary Backpack.tf Fix
-Fixes the order of Unusuals at https://backpack.tf/unusual/ and enables sorting again.
+Fixes the order of Unusuals at https://backpack.tf/unusual/<unusual> and https://backpack.tf/unusuals/.
+Additionally you can sort by other values (including price, number in existance, name, effect-id) and the filter input field is fixed.
+
 
 ## Installation
 Use an extension which can execute userscripts (F.e. [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) for [Chrome](https://www.google.com/chrome/) or [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/)  for [Firefox](https://www.mozilla.org/firefox))
 and then install using this link: [Temporary Backpack.tf Fix.user.js](https://github.com/NetroScript/Backpack.tf-Unusual-Sorting-Fix/raw/master/Temporary%20Backpack.tf%20Fix.user.js).
 
 (Or paste / install it manually for your plugin)
+
+## Changelog
+
+# 0.2
+
+Added:
+* Also sort on https://backpack.tf/unusuals
+* Fixed Filter items field
 
 ## Preview
 
--- Temporary Backpack.tf Fix.user.js
+++ Temporary Backpack.tf Fix.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Temporary Backpack.tf Unusual Sorting Fix
 // @namespace    https://github.com/NetroScript
-// @version      0.1
+// @version      0.2
 // @description  Fixes the order of Unusuals at https://backpack.tf/unusual/ + enables sorting again
 // @author       Netroscript
 // @match        https://backpack.tf/unusual/*
 // @match        http://backpack.tf/unusual/*
+// @include      /^(http|https)://backpack.tf/unusual/
 // @require      http://code.jquery.com/jquery-2.2.4.min.js
 // @run-at       document-end
 // @grant        none
@@ -16,7 +17,59 @@
 	'use strict';
 
 	var items = $("li.item");
-	var css = `
+
+	var url = document.createElement("a");
+	url.href = window.location.href;
+
+
+	if(url.pathname=="/unusuals" || url.pathname=="/unusuals/"){
+
+
+		var filtervar = "Default";
+		function filteri(){
+			$(items).show();
+			$(items).filter(function(){
+				var i = $(this).attr('data-class');
+				if (typeof i == 'undefined' && filtervar == "Multi") {
+					return false;
+				}
+				if (typeof i == 'undefined') {
+					return true;
+				}
+				return !i.includes(filtervar);
+			}).hide();
+		}
+
+
+		var cla = $("#classmenu a");
+		for(var i = 0; i < cla.length; i++){
+
+			$(cla[i])[0].onclick = null;
+			$(cla[i]).removeAttr("onclick");
+
+		}
+
+
+		$("#classmenu a").click(function(e){
+
+			$("#className").text($(e.target).text());
+			filtervar = $(e.target).attr("data-class");
+			filteri();
+			if(filtervar == "All") $(items).show();
+
+		});
+
+
+
+	}
+	
+
+
+	if (url.pathname.startsWith("/unusual")){
+
+
+
+		var css = `
 <style>
 .sbuttoncon {
 position: fixed;
@@ -36,70 +89,81 @@ cursor: pointer;
 }
 </style>
 `;
-	var html = `<div class="sbuttoncon">
+		var html = `<div class="sbuttoncon">
 <div class="sbutton" id="sbp">Sort by Price</div>
 <div class="sbutton" id="sbe">Sort by Number in existance</div>
 <div class="sbutton" id="sbn">Sort by Name</div>
 <div class="sbutton" id="sbei">Sort by Effect-ID</div>
 </div>`;
 
-	$("#page-content").prepend(html);
-	$("head").append(css);
+		$("#page-content").prepend(html);
+		$("head").append(css);
 
-	var sortType = "data-price";
-	var osortType = "data-price";
-	var toggle = 0;
-	var text = false;
+		var sortType = "data-price";
+		var osortType = "data-price";
+		var toggle = 0;
+		var text = false;
 
-	function sortbV(){
+		
+		
+		$('#filterlist').on('keyup', function(e){
+			var filterValue = $('#filterlist').val();
+			$(items).show()
+				.filter(function(){
+				return !$(this).attr('title').includes(filterValue);
+			}).hide();
+
+		});
+		
+		function sortbV(){
 
 
 
-		items.sort(function (a, b) {
+			items.sort(function (a, b) {
 
 
 
+				if(toggle && sortType == osortType){
+					var tmp = a;
+					a = b;
+					b = tmp;
+				}
+
+				if(text){
+					if($(a).attr(sortType).toLowerCase() < $(b).attr(sortType).toLowerCase()) return -1;
+					if($(a).attr(sortType).toLowerCase() > $(b).attr(sortType).toLowerCase()) return 1;
+					return 0;
+				}
+				return parseFloat($(b).attr(sortType)) - parseFloat($(a).attr(sortType)) ;
+
+			})
+				.appendTo("#unusual-pricelist");
 			if(toggle && sortType == osortType){
-				var tmp = a;
-				a = b;
-				b = tmp;
-			}
+				toggle=false;
+			}else toggle = true;
+			osortType = sortType;
+		};
 
-			if(text){
-				if($(a).attr(sortType).toLowerCase() < $(b).attr(sortType).toLowerCase()) return -1;
-				if($(a).attr(sortType).toLowerCase() > $(b).attr(sortType).toLowerCase()) return 1;
-				return 0;
-			}
-			return parseFloat($(b).attr(sortType)) - parseFloat($(a).attr(sortType)) ;
-
-		})
-			.appendTo("#unusual-pricelist");
-		if(toggle && sortType == osortType){
-			toggle=false;
-		}else toggle = true;
-		osortType = sortType;
-	};
-
-	sortbV();
-
-	$("#sbp").click(function(){
-		sortType = "data-price";
 		sortbV();
-	});
-	$("#sbe").click(function(){
-		sortType = "data-exist";
-		sortbV();
-	});
-	$("#sbn").click(function(){
-		sortType = "title";
-		text = true;
-		sortbV();
-		text = false;
-	});
-	$("#sbei").click(function(){
-		sortType = "data-effect_id";
-		sortbV();
-	});
 
+		$("#sbp").click(function(){
+			sortType = "data-price";
+			sortbV();
+		});
+		$("#sbe").click(function(){
+			sortType = "data-exist";
+			sortbV();
+		});
+		$("#sbn").click(function(){
+			sortType = "title";
+			text = true;
+			sortbV();
+			text = false;
+		});
+		$("#sbei").click(function(){
+			sortType = "data-effect_id";
+			sortbV();
+		});
+	}
 
 })();
